bc2b4572f7742fba56e266ade5fc57e61220e762
Nas <nas.shihab92@gmail.com>
2023-10-19 13:04:22 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/camera_bar_code/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.camera_bar_code
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/homepage.dart
@@ -0,0 +1,50 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  File? _pickedImage;
+
+  Future<void> _pickImageFromCamera() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.camera);
+
+    if (pickedFile != null) {
+      setState(() {
+        _pickedImage = File(pickedFile.path);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 25),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if (_pickedImage != null)
+                Image.file(
+                  _pickedImage!,
+                ),
+              Center(
+                  child: ElevatedButton(
+                onPressed: _pickImageFromCamera,
+                child: const Text("Pick Image from Camera"),
+              ))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
