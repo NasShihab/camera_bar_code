f68131a88d9c78d0607bce9c6ac6bf8a09c24731
Nas <nas.shihab92@gmail.com>
2023-10-22 11:03:35 +0600

    barcode ok

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,6 +1,4 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
 
 import 'image_scan.dart';
 
@@ -12,61 +10,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  File? _pickedImage;
-
-  Future<void> _pickImageFromCamera() async {
-    final picker = ImagePicker();
-    final pickedFile = await picker.pickImage(source: ImageSource.camera);
-
-    if (pickedFile != null) {
-      setState(() {
-        _pickedImage = File(pickedFile.path);
-      });
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 25),
+          padding: const EdgeInsets.symmetric(horizontal: 25),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              if (_pickedImage != null)
-                Image.file(
-                  _pickedImage!,
-                ),
-              Center(
-                child: ElevatedButton(
-                  onPressed: _pickImageFromCamera,
-                  child: const Text("Pick Image from Camera"),
-                ),
-              ),
-              Center(
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: const Text("2nd"),
-                ),
-              ),
-              Center(
-                child: ElevatedButton(
-                  onPressed: () {
-                    // Navigator.push(context, MaterialPageRoute(builder: (context) => const QQrCodeScanView()));
-                  },
-                  child: const Text("3rd"),
-                ),
-
-              ),
               Center(
                 child: ElevatedButton(
                   onPressed: () {
                     Navigator.push(context, MaterialPageRoute(builder: (context) => const CaptureThenScan()));
                   },
-                  child: const Text("CaptureThenScan"),
+                  child: const Text("Capture Then Scan"),
                 ),
-
               ),
             ],
           ),
