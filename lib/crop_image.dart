25af8a62ff3ac58ed335056bcb2239f5cad33af5
Nas <nas.shihab92@gmail.com>
2023-10-22 13:42:05 +0600

    pick and crop

---
+++ lib/crop_image.dart
@@ -0,0 +1,109 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
+
+class CropImage extends StatefulWidget {
+  const CropImage({Key? key}) : super(key: key);
+
+  @override
+  State<CropImage> createState() => _CropImageState();
+}
+
+class _CropImageState extends State<CropImage> {
+  File? imageFile;
+
+  Future pickImage() async {
+    final pickedImage =
+    await ImagePicker().pickImage(source: ImageSource.gallery);
+
+    if (pickedImage != null) {
+      setState(() {
+        imageFile = File(pickedImage.path);
+      });
+    }
+  }
+
+  Future cropImage() async {
+    if (imageFile != null) {
+      CroppedFile? cropped = await ImageCropper().cropImage(
+          sourcePath: imageFile!.path,
+          aspectRatioPresets:
+          [
+            CropAspectRatioPreset.square,
+            CropAspectRatioPreset.ratio3x2,
+            CropAspectRatioPreset.original,
+            CropAspectRatioPreset.ratio4x3,
+            CropAspectRatioPreset.ratio16x9
+          ],
+          uiSettings: [
+          AndroidUiSettings(
+          toolbarTitle: 'Crop',
+          cropGridColor: Colors.black,
+          initAspectRatio: CropAspectRatioPreset.original,
+          lockAspectRatio: false),
+          IOSUiSettings(title: 'Crop')
+    ]);
+
+    if (cropped != null) {
+    setState(() {
+    imageFile = File(cropped.path);
+    });
+    }
+  }
+  }
+
+  void _clearImage() {
+    setState(() {
+      imageFile = null;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            backgroundColor: Colors.black,
+            title: const Text("Crop Your Image")),
+        body: Column(
+          children: [
+            Expanded(
+                flex: 3,
+                child: imageFile != null
+                    ? Container(
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: Image.file(imageFile!))
+                    : const Center(
+                  child: Text("Add a picture"),
+                )),
+            Expanded(
+                child: Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      _buildIconButton(icon: Icons.add, onpressed: pickImage),
+                      _buildIconButton(icon: Icons.crop, onpressed: cropImage),
+                      _buildIconButton(icon: Icons.clear, onpressed: _clearImage),
+                    ],
+                  ),
+                ))
+          ],
+        ));
+  }
+
+  Widget _buildIconButton(
+      {required IconData icon, required void Function()? onpressed}) {
+    return Container(
+        width: 50,
+        height: 50,
+        decoration: BoxDecoration(
+            color: Colors.black, borderRadius: BorderRadius.circular(10)),
+        child: IconButton(
+          onPressed: onpressed,
+          icon: Icon(icon),
+          color: Colors.white,
+        ));
+  }
+}
