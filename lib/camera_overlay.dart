f7ddd761d66ede010bf437ee67e142be87b4b816
Nas <nas.shihab92@gmail.com>
2023-10-23 17:24:00 +0600

    pick and crop

--- lib/camera_overlay.dart
+++ lib/camera_overlay.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
 import 'package:image/image.dart' as img;
 
 class CameraScreen extends StatefulWidget {
@@ -69,8 +70,15 @@ class CameraScreenState extends State<CameraScreen> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const Center(child: Text('দয়াকরে কার্ডটি ফ্রেমের ভিতরে রাখুন', style: TextStyle(fontSize: 16, color: Colors.yellow),textAlign: TextAlign.center,)),
-                const SizedBox(height: 20,),
+                const Center(
+                    child: Text(
+                  'দয়াকরে কার্ডটি ফ্রেমের ভিতরে রাখুন',
+                  style: TextStyle(fontSize: 16, color: Colors.yellow),
+                  textAlign: TextAlign.center,
+                )),
+                const SizedBox(
+                  height: 20,
+                ),
                 Container(
                   width: screenSize.width * 0.90,
                   height: screenSize.height * 0.25,
@@ -81,7 +89,7 @@ class CameraScreenState extends State<CameraScreen> {
                     ),
                   ),
                 ),
-                const SizedBox(height: 20),
+                const SizedBox(height: 50),
                 FloatingActionButton(
                   onPressed: () {
                     _captureCard();
@@ -108,17 +116,73 @@ class DisplayCapturedImage extends StatefulWidget {
 }
 
 class DisplayCapturedImageState extends State<DisplayCapturedImage> {
+  String extractedBarcode = '';
+
+  Future<void> recognizedText(String pickedImage) async {
+    if (pickedImage == null) {
+      debugPrint("Error 2");
+    } else {
+      setState(() {
+        extractedBarcode = '';
+      });
+      var barCodeScanner = GoogleMlKit.vision.barcodeScanner();
+      final visionImage = InputImage.fromFilePath(pickedImage);
+      try {
+        var barcodeText = await barCodeScanner.processImage(visionImage);
+
+        for (Barcode barcode in barcodeText) {
+          setState(() {
+            // extractedBarcode = barcode.displayValue!;
+            extractedBarcode += "${barcode.displayValue!}\n";
+          });
+        }
+        // if (barcodeText.isEmpty) {
+        //   debugPrint("No barcodes found in the image");
+        // }
+        // else {
+        //   for (Barcode barcode in barcodeText) {
+        //     if (barcode.displayValue != null && barcode.displayValue!.length == 26) {
+        //       setState(() {
+        //         extractedBarcode = barcode.displayValue!;
+        //       });
+        //     }
+        //   }
+        // }
+      } catch (e) {
+        debugPrint("Error");
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Cropped Image'),
       ),
-      body: Center(
-        child: Image.file(
-          File(widget.imagePath),
-          fit: BoxFit.cover,
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Image.file(
+              File(widget.imagePath),
+              height: MediaQuery.of(context).size.height * .45,
+              width: 300,
+              fit: BoxFit.cover,
+            ),
+          ),
+          extractedBarcode.isEmpty
+              ? const Text("No data found in barcode")
+              : Text(
+                  extractedBarcode,
+                  textAlign: TextAlign.center,
+                ),
+          ElevatedButton(
+              onPressed: () {
+                recognizedText(widget.imagePath);
+              },
+              child: Text("QR"))
+        ],
       ),
     );
   }
