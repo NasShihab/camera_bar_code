274cd211edd6e10de9f4238bfbbc9156923d838e
Nas Shihab <nas.shihab92@gmail.com>
2023-11-15 14:48:40 +0600

    Animatade

--- lib/animation_cross_fade/animated_cross_fade.dart
+++ lib/animation_cross_fade/animated_cross_fade.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class AnimatedCrossFadeTest extends StatefulWidget {
   const AnimatedCrossFadeTest({super.key});
 
@@ -9,18 +8,72 @@ class AnimatedCrossFadeTest extends StatefulWidget {
 }
 
 class _AnimatedCrossFadeTestState extends State<AnimatedCrossFadeTest> {
+  int selectedIndex = 1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                ElevatedButton(onPressed: (){}, child: Text('Button 1'),),
-                ElevatedButton(onPressed: (){}, child: Text('Button 1'),),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      selectedIndex = 1;
+                    });
+                  },
+                  child: const Text('Button 1'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      selectedIndex = 2;
+                    });
+                  },
+                  child: const Text('Button 1'),
+                ),
               ],
-            )
+            ),
+            AnimatedCrossFade(
+                firstChild: const Column(
+                  children: [
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Chip(
+                      label: Text('Text 1'),
+                      backgroundColor: Colors.green,
+                    ),
+                  ],
+                ),
+                secondChild: const Column(
+                  children: [
+                    Chip(
+                      label: Text('Text 2'),
+                      backgroundColor: Colors.red,
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                  ],
+                ),
+                firstCurve: Curves.linear,
+                secondCurve: Curves.linearToEaseOut,
+                crossFadeState: selectedIndex == 1 ? CrossFadeState.showFirst : CrossFadeState.showSecond,
+                duration: const Duration(seconds: 1)),
+            // const Chip(
+            //   label: Text('Text 1'),
+            //   backgroundColor: Colors.green,
+            // ),
+            // const Chip(
+            //   label: Text('Text 2'),
+            //   backgroundColor: Colors.red,
+            // ),
           ],
         ),
       ),
